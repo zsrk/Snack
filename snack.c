ce17f4c9e935e2a1e308bdb6e5dff55df8a95822
ZSRK <azusorak@gmail.com>
2021-02-16 23:28:56 +0000

    Added 'grass' above rPlayArea.

--- snack.c
+++ snack.c
@@ -33,6 +33,8 @@ int main( int argc, char* argv[] )
 
     SDL_Rect rPlayArea ;
 
+    SDL_Rect rGrass ;
+
     SDL_Event eKeyboardInput ;
 
     SDL_bool boGameLoop = SDL_TRUE ;
@@ -60,6 +62,11 @@ int main( int argc, char* argv[] )
     rPlayArea.h  = dmDisplayMode.h - ( rPlayArea.y * 2 ) ;
     rPlayArea.w = dmDisplayMode.h ;
 
+    rGrass.y = 0 ;
+    rGrass.x = 0 ;
+    rGrass.h = dmDisplayMode.h / 50 ;
+    rGrass.w = dmDisplayMode.w ;
+
     whHungryWorm.rHead.x = dmDisplayMode.w / 2 ;
     whHungryWorm.rHead.y = dmDisplayMode.h / 2 ;
     whHungryWorm.rHead.h = rPlayArea.h / 15 ;
@@ -73,11 +80,20 @@ int main( int argc, char* argv[] )
 
     SDL_FillRect( srfcScreen, &rPlayArea, SDL_MapRGB( srfcScreen->format, 127, 72, 7 ) ) ;
 
-    // Manually setting a pixel!
+    SDL_FillRect( srfcScreen, &rGrass, SDL_MapRGB( srfcScreen->format, 0, 255, 0 ) ) ;
+
+    /* Manually setting a pixel!
 
     Uint32* pix = (Uint32*)srfcScreen->pixels ;
 
-    pix[( 5 * srfcScreen->w ) + 5] = 0xFFFF0000 ; 
+    for( int i = rPlayArea.x; i < rPlayArea.w ; i++ )
+    {
+        pix[i] = 0xFFFF0000 ;
+    }
+
+    */
+
+    //SDL_memset(srfcScreen->pixels, SDL_MapRGBA( srfcScreen->format, 255, 0, 0, 0 ), ( dmDisplayMode.h / 50 )  * srfcScreen->pitch);
 
     //SDL_FillRect( srfcSurface, &rDisplayBounds, SDL_MapRGB( srfcSurface->format, 0, 0, 255 ) ) ;
     // Having the taskbar at top of screen, and using a maximised window, results in a loss of screen space when using
